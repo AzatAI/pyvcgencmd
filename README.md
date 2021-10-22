1ab68cd0b39656ab30effdc64daa8ab18e320309
Yaakov Azat <yaakovazat@gmail.com>
2021-10-22 15:37:19 +0600

    updated readme

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Python Vcgencmd Wrapper
 
-Python Wrapper for BCM (Raspberry Pi) vcgencmd
+Python Wrapper for BCM (Raspberry Pi) vcgencmd.
+
+This project initially created for AzatAI Xlink Clusters, where we use Raspbeery Pis as a node for our kubernets classters. They worked very well, to control and monitor the rpi nodes, we then created this package.
+
+This package is also used in many other projects where RPi is a means of central computational power.
+
+
 
 Still experimental.
 
--- setup.py
+++ setup.py
@@ -11,61 +11,60 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 
 # 'setup.py publish' shortcut.
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist')
-    os.system('twine upload dist/*')
-    os.system('rm -rf dist')
-    os.system('rm -rf *.egg-info')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist")
+    os.system("twine upload dist/*")
+    os.system("rm -rf dist")
+    os.system("rm -rf *.egg-info")
     sys.exit()
 
 
-requires = [
-]
+requires = ["pydantic", "psutil"]
 
 
 about = {}  # create a empty dictionary to store the content of the __version__.py file
 # execute the __version__.py file and get the content, save to the dictionary about.
-with open(os.path.join(here, 'src', '__version__.py'), 'r', 'utf-8') as f:
+with open(os.path.join(here, "src", "__version__.py"), "r", "utf-8") as f:
     exec(f.read(), about)
 
-with open('README.md', 'r', 'utf-8') as f:
+with open("README.md", "r", "utf-8") as f:
     readme = f.read()
 
 setup(
-    name=about['__title__'],
-    version=about['__version__'],
-    description=about['__description__'],
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author=about['__author__'],
-    author_email=about['__author_email__'],
-    url=about['__url__'],
+    long_description_content_type="text/markdown",
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    url=about["__url__"],
     packages=find_packages(),
-    package_data={'': ['LICENSE']},
+    package_data={"": ["LICENSE"]},
     # package_dir={'startpkg': 'src'},
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=requires,
     # entry_points='''
     # ''',
-    license=about['__license__'],
+    license=about["__license__"],
     zip_safe=False,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     project_urls={
-        'Documentation': 'https://azat.ai',
-        'Source': 'https://github.com/AzatAI/pyvcgencmd',
+        "Documentation": "https://azat.ai",
+        "Source": "https://github.com/AzatAI/pyvcgencmd",
     },
 )
